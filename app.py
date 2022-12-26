a33b1b30f1daefc0704d8ac3af55641a173e695c
Samal <evgeniy.samal@mail.ru>
2022-12-26 15:26:37 +0300

    оптимизировал шаблон, добавил БД

--- app.py
+++ app.py
@@ -1,39 +1,57 @@
-from flask import Flask,render_template, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    description = db.Column(db.Text, nullable = False)
-    price = db.Column(db.Integer, nullable = False)
-    isActive = db.Column(db.Boolean, default = True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
 
+    def __repr__(self):
+        return self.title
 
 @app.route('/')
-def index ():
+def index():
     return render_template('index.html')
 
 
 @app.route('/about')
-def about ():
+def about():
     return render_template('about.html')
 
 
-@app.route('/create')
-def create ():
-    return render_template('create.html')
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        description = request.form['description']
+        item = Item(title=title, price=price, description=description)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect("/")
+
+        except:
+            return "Произошла ошибка при добавлении"
+
+    else:
+        return render_template('create.html')
 
 
 @app.route('/detail')
-def detail ():
+def detail():
     return render_template('detail.html')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
+    db.create_all()
